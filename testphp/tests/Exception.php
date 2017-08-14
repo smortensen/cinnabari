c166ef1998e797baa3123332f1492155b3422147
Mark Greeley <mgreeley@datto.com>
2017-08-14 10:34:19 -0400

    Update Exception tests to track changes to Exception

--- testphp/tests/Exception.php
+++ testphp/tests/Exception.php
@@ -8,14 +8,13 @@ require TESTPHP . '/autoload.php';
 // Test
 throw Exception::invalidSyntax('parameter', $input, 1);
 
-
 // Input
 $input = ':*';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => ':*', 'line' => 1, 'character' => 2, 'expected' => 'parameter'),
     'Expected parameter identifier after \':\', found \'*\' instead on line 1, character 2'
 );
 
@@ -23,14 +22,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('end', $input, 2, ':x');
 
-
 // Input
 $input = ':x ';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => ':x ', 'line' => 1, 'character' => 3, 'expected' => 'end'),
     'Expected end of input after \':x\', found \'\' instead on line 1, character 3'
 );
 
@@ -38,14 +36,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('expression', $input, 0);
 
-
 // Input
 $input = '.';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '.', 'line' => 1, 'character' => 1, 'expected' => 'expression'),
     'Expected expression, found \'.\' instead on line 1, character 1'
 );
 
@@ -53,14 +50,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('property', $input, 3, '.');
 
-
 // Input
 $input = 'x .';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => 'x .', 'line' => 1, 'character' => 4, 'expected' => 'property'),
     'Expected property identifier after \'.\', found \'\' instead on line 1, character 4'
 );
 
@@ -68,14 +64,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('argument', $input, 2);
 
-
 // Input
 $input = 'f(*)';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => 'f(*)', 'line' => 1, 'character' => 3, 'expected' => 'argument'),
     'Expected function argument, found \'*)\' instead on line 1, character 3'
 );
 
@@ -83,18 +78,19 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('argument', $input, 6, ':x, ');
 
-
 // Input
 $input = 'f(:x, *)';
 
 // Output
-throw new Exception(Exception::QUERY_INVALID_SYNTAX, $input, 'Expected function argument '
-    . 'after \':x, \', found \'*)\' instead on line 1, character 7');
+throw new Exception(Exception::QUERY_INVALID_SYNTAX,
+    array('statement' => 'f(:x, *)', 'line' => 1, 'character' => 7, 'expected' => 'argument'),
+    'Expected function argument '
+    . 'after \':x, \', found \'*)\' instead on line 1, character 7'
+);
 
 
 // Test
 throw Exception::invalidSyntax('function-comma', $input, 5, 'abc');
-
 
 // Input
 $input = 'f(abc';
@@ -102,7 +98,7 @@ $input = 'f(abc';
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => 'f(abc', 'line' => 1, 'character' => 6, 'expected' => 'function-comma'),
     'Expected \', \' or \')\' after \'abc\', found \'\' instead on line 1, character 6'
 );
 
@@ -110,14 +106,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('argument', $input, 7, 'abc, ');
 
-
 // Input
 $input = 'f(abc, ';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => 'f(abc, ', 'line' => 1, 'character' => 8, 'expected' => 'argument'),
     'Expected function argument after \'abc, \', found \'\' instead on line 1, character 8'
 );
 
@@ -125,14 +120,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('group-expression', $input, 1);
 
-
 // Input
 $input = '(';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '(', 'line' => 1, 'character' => 2, 'expected' => 'group-expression'),
     'Expected expression in group, found \'\' instead on line 1, character 2'
 );
 
@@ -140,14 +134,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('object-element', $input, 1, '{');
 
-
 // Input
 $input = '{}';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '{}', 'line' => 1, 'character' => 2, 'expected' => 'object-element'),
     'Expected key/value pair (e.g. "name": property) after \'{\', '
     . 'found \'}\' instead on line 1, character 2'
 );
@@ -156,14 +149,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('object-comma', $input, 2, 'x');
 
-
 // Input
 $input = '{x';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '{x', 'line' => 1, 'character' => 3, 'expected' => 'object-comma'),
     'Expected \', \' or \'}\' after \'x\', found \'\' instead on line 1, character 3'
 );
 
@@ -171,14 +163,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('object-element', $input, 4, 'x, ');
 
-
 // Input
 $input = '{x, }';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '{x, }', 'line' => 1, 'character' => 5, 'expected' => 'object-element'),
     'Expected key/value pair (e.g. "name": property) after \'x, \', '
     . 'found \'}\' instead on line 1, character 5'
 );
@@ -187,14 +178,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('pair-colon', $input, 4, '"x"');
 
-
 // Input
 $input = '{"x" x}';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '{"x" x}', 'line' => 1, 'character' => 5, 'expected' => 'pair-colon'),
     "Expected ':' after '\"x\"', found 'x}' instead on line 1, character 5"
 );
 
@@ -202,14 +192,13 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('pair-property', $input, 6, '"x":');
 
-
 // Input
 $input = '{"x": *}';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => '{"x": *}', 'line' => 1, 'character' => 7, 'expected' => 'pair-property'),
     'Expected property expression after \'"x":\', found \'*}\' instead on line 1, character 7'
 );
 
@@ -217,13 +206,12 @@ throw new Exception(
 // Test
 throw Exception::invalidSyntax('unary-expression', $input, 7, ' + ');
 
-
 // Input
 $input = 'test + *';
 
 // Output
 throw new Exception(
     Exception::QUERY_INVALID_SYNTAX,
-    $input,
+    array('statement' => 'test + *', 'line' => 1, 'character' => 8, 'expected' => 'unary-expression'    ),
     'Expected unary-expression after \' + \' operator, found \'*\' instead on line 1, character 8'
 );
